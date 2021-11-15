88c91e39047969d4da5e7fedc2e8449c6ed5edec
cherideal <101du@163.com>
2021-11-15 17:12:17 +0800

    draft 202111151628

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -165,9 +165,6 @@ library HybridLibrary {
 library UniswapV2Library {
     using SafeMath for uint;
 
-    uint internal constant LIMIT_BUY = 1;
-    uint internal constant LIMIT_SELL = 2;
-
     // returns sorted token addresses, used to handle return values from pairs sorted in this order
     function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
         require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
