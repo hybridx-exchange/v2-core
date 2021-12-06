5ab3b44abcfa328e43940a4b3e1582cb63640f4d
cherideal <101du@163.com>
2021-12-06 18:13:42 +0800

    Decoupling from orderbook function

--- contracts/interfaces/IUniswapV2Factory.sol
+++ contracts/interfaces/IUniswapV2Factory.sol
@@ -11,7 +11,7 @@ interface IUniswapV2Factory {
     function allPairsLength() external view returns (uint);
 
     function createPair(address tokenA, address tokenB) external returns (address pair);
-    function getOrderBookFactory() external view returns (address factory);
+    function getOrderBookFactory() external view returns (address);
     function setOrderBookFactory(address _orderBookFactory) external;
     function getCodeHash() external pure returns (bytes32);
 
